af822689d1c52720076b9e341311f66f6b3ac49d
muhozadenyse9 <muhozadenyse9@gmail.com>
2026-02-15 15:24:09 +0200

    added score global in js

---
+++ script.js
@@ -0,0 +1,13 @@
+// ==========================
+// GLOBAL SCORE
+// ==========================
+let score = 0;
+
+const scoreDisplay = document.querySelector("#scoreDisplay");
+const likeButtons = document.querySelectorAll(".like-btn");
+const searchInput = document.querySelector("#searchInput");
+const posts = document.querySelectorAll(".blog-post");
+const tags = document.querySelectorAll(".tag");
+const backToTop = document.querySelector("#backToTop");
+const navLinks = document.querySelectorAll(".nav-link");
+const mainTitle = document.querySelector("#mainTitle");
